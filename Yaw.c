99b5edfbfc39b2e0413be2d268c4dd3a4f60d655
Thomas Forsey <tfo49@uclive.ac.nz>
2024-04-23 15:57:06 +1200

    add yaw interrupt handler and degree conversion functions

--- Yaw.c
+++ Yaw.c
@@ -6,5 +6,45 @@
  */
 
 
+void initYaw(){
 
+}
 
+void YawInterruptHandler(){
+    // read GPIO pins into chAState & chBState
+
+    state = state << NIBBLE_SIZE;
+
+    if(chAState){
+        if(chBState)
+           state += 2;
+        else
+           state += 3;
+    }
+    else{
+        if(chBState)
+            state += 1;
+        else
+            state += 0;
+    }
+
+    calculateYaw();
+}
+
+void calculateYaw(){
+    //  And operation masks previous state, right shift operation
+    if ((state && NIBBLE_SIZE) == ((state >> NIBBLE_SIZE) + 1)) //encoder is turning clockwise
+        numPhaseChanges++;
+    else // encoder is turning anti-clockwise
+        numPhaseChanges--;
+
+    numPhaseChanges %= (NUM_ENCODER_TEETH * NUM_PHASES);
+}
+
+int16_t getYawDegrees(){
+    return (DEG_PER_PHASE_X100 * numPhaseChanges - 180) / 100;
+}
+
+uint8_t getYawDecimal(){
+    return (uint8_t) ((DEG_PER_PHASE_X100 * numPhaseChanges - 180) % 100;
+}
--- Yaw.h
+++ Yaw.h
@@ -8,6 +8,47 @@
 #ifndef YAW_H_
 #define YAW_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/adc.h"
+#include "driverlib/pwm.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/debug.h"
+#include "utils/ustdlib.h"
+#include "OrbitOLED/OrbitOLEDInterface.h"
+#include "stdlib.h"
+#include "buttons4.h"
+#include "circBufT.h"
+#include "inc/hw_ints.h"  // Interrupts
+
+#define NIBBLE_SIZE 4
+
+#define NUM_ENCODER_TEETH 112
+#define NUM_PHASES 4
+#define DEG_PER_PHASE_X100 81
+#define SCALE_FACTOR 100
+
+static bool chAState = 0;
+static bool chBState = 0;
+
+/*
+ * upper 4 bits are used for previous state, lower four bits are used for current state
+ * to change state, shift left 4 times, then just add phase value (00 = 0, 01 = 1, 11 = 2, 10 = 3)
+ */
+static uint8_t state;
+
+static int16_t numPhaseChanges;
+
+void initYaw();
+void YawInterruptHandler();
+void calculateYaw();
+int16_t getYawDegrees();
+
 
 
 
