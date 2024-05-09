167c73f74f5941878fadc1753c68aa15531377fc
tfo49 <tfo49@uclive.ac.nz>
2024-05-09 19:24:53 +1200

    Add switch functionality to buttons module

--- buttons4.c
+++ buttons4.c
@@ -24,8 +24,6 @@
 #include "driverlib/debug.h"
 #include "inc/tm4c123gh6pm.h"  // Board specific defines (for PF0)
 #include "buttons4.h"
-
-
 // *******************************************************
 // Globals to module
 // *******************************************************
@@ -78,9 +76,6 @@ initButtons (void)
     GPIOPinTypeGPIOInput(SWITCH1_PORT_BASE, SWITCH1_PIN);
     GPIOPadConfigSet(SWITCH1_PORT_BASE, SWITCH1_PIN, GPIO_STRENGTH_2MA,
                      GPIO_PIN_TYPE_STD_WPD);
-    GPIOIntRegister(SWITCH1_PORT_BASE, SwitchIntHandler);
-    GPIOIntTypeSet(SWITCH1_PORT_BASE, SWITCH1_PIN, GPIO_BOTH_EDGES);
-    GPIOIntEnable(SWITCH1_PORT_BASE, SWITCH1_PIN);
     but_normal[SWITCH1] = SWITCH1_NORMAL;
 
 	for (i = 0; i < NUM_BUTS; i++)
@@ -111,6 +106,8 @@ updateButtons (void)
 	but_value[DOWN] = (GPIOPinRead (DOWN_BUT_PORT_BASE, DOWN_BUT_PIN) == DOWN_BUT_PIN);
     but_value[LEFT] = (GPIOPinRead (LEFT_BUT_PORT_BASE, LEFT_BUT_PIN) == LEFT_BUT_PIN);
     but_value[RIGHT] = (GPIOPinRead (RIGHT_BUT_PORT_BASE, RIGHT_BUT_PIN) == RIGHT_BUT_PIN);
+    but_value[SWITCH1] = (GPIOPinRead (SWITCH1_PORT_BASE, SWITCH1_PIN) == SWITCH1_PIN);
+
 	// Iterate through the buttons, updating button variables as required
 	for (i = 0; i < NUM_BUTS; i++)
 	{
@@ -133,9 +130,7 @@ updateButtons (void)
 //// checkButton: Function returns the new button logical state if the button
 //// logical state (PUSHED or RELEASED) has changed since the last call,
 //// otherwise returns NO_CHANGE.
-uint8_t
-checkButton (uint8_t butName)
-{
+uint8_t checkButton (uint8_t butName) {
 	if (but_flag[butName])
 	{
 		but_flag[butName] = false;
@@ -147,5 +142,7 @@ checkButton (uint8_t butName)
 	return NO_CHANGE;
 }
 
-void buttonInterrupt
+uint8_t checkSwitch(uint8_t switchName){
+    return (but_state[switchName] == but_normal[switchName]) ? INACTIVE : ACTIVE;
+}
 
--- buttons4.h
+++ buttons4.h
@@ -20,8 +20,9 @@
 //*****************************************************************************
 // Constants
 //*****************************************************************************
-enum butNames {UP = 0, DOWN, LEFT, RIGHT, NUM_BUTS};
+enum butNames {UP = 0, DOWN, LEFT, RIGHT, SWITCH1, NUM_BUTS};
 enum butStates {RELEASED = 0, PUSHED, NO_CHANGE};
+enum switchStates {INACTIVE = 0, ACTIVE};
 // UP button
 #define UP_BUT_PERIPH  SYSCTL_PERIPH_GPIOE
 #define UP_BUT_PORT_BASE  GPIO_PORTE_BASE
@@ -46,7 +47,7 @@ enum butStates {RELEASED = 0, PUSHED, NO_CHANGE};
 #define SWITCH1_PERIPH SYSCTL_PERIPH_GPIOA
 #define SWITCH1_PORT_BASE GPIO_PORTA_BASE
 #define SWITCH1_PIN GPIO_PIN_7
-#define SWITCH1_NORMAL true
+#define SWITCH1_NORMAL false
 
 #define NUM_BUT_POLLS 3
 // Debounce algorithm: A state machine is associated with each button.
@@ -57,23 +58,23 @@ enum butStates {RELEASED = 0, PUSHED, NO_CHANGE};
 // *******************************************************
 // initButtons: Initialise the variables associated with the set of buttons
 // defined by the constants above.
-void
-initButtons (void);
+void initButtons (void);
 
 // *******************************************************
 // updateButtons: Function designed to be called regularly. It polls all
 // buttons once and updates variables associated with the buttons if
 // necessary.  It is efficient enough to be part of an ISR, e.g. from
 // a SysTick interrupt.
-void
-updateButtons (void);
+void updateButtons (void);
 
 // *******************************************************
 // checkButton: Function returns the new button state if the button state
 // (PUSHED or RELEASED) has changed since the last call, otherwise returns
 // NO_CHANGE.  The argument butName should be one of constants in the
 // enumeration butStates, excluding 'NUM_BUTS'. Safe under interrupt.
-uint8_t
-checkButton (uint8_t butName);
+uint8_t checkButton (uint8_t butName);
+
+uint8_t checkSwitch(uint8_t switchName){
+
 
 #endif /*BUTTONS_H_*/
